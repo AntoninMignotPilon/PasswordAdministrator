f3b5c7e184047a1440c2a6b1cac6e425d73f1427
AntoninMignotPilon <antonin.mignotpilon@viacesi.fr>
2025-07-23 14:42:52 +0200

    Ajout du menu de choix

--- src/main/java/org/example/GestionnaireMdp.java
+++ src/main/java/org/example/GestionnaireMdp.java
@@ -16,4 +16,25 @@ public class GestionnaireMdp {
 
         }
 
+        boolean actif = true;
+
+        while (actif){
+            System.out.println("-----Gestionnaire de Mots De Passes-----");
+            System.out.println("1. Lister les mots de passe");
+            System.out.println("2. Ajouter un mot de passe");
+            System.out.println("3. Supprimer un mot de passe");
+            System.out.println("4. Quitter");
+            System.out.print("> ");
+            String choix = scanner.nextLine();
+
+
+            switch (choix){
+                case "1" -> stockage.afficherTous();
+                case "2" -> stockage.ajouter(scanner);
+                case "3" -> stockage.supprimer(scanner);
+                case "4" -> actif = false;
+                default -> System.out.println("Choi non reconnu");
+            }
+        }
+
 }
